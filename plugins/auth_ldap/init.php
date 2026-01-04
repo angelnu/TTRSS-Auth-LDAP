41974fb0bb289bcae2a74de6cba74d5b05c902db
Angel Nunez Mencias <git@angelnu.com>
2026-01-04 00:16:11 +0000

    step up version

--- plugins/auth_ldap/init.php
+++ plugins/auth_ldap/init.php
@@ -7,7 +7,7 @@
  * @author angelnu (git@angelnu.com)
  * @copyright GPL2
  *  Requires php-ldap 
- * @version 3.00
+ * @version 3.01
  */
 
 /**
@@ -85,7 +85,7 @@ class Auth_Ldap extends Auth_Base {
     private $_debugMode;
 
     function about() {
-        return array(3.00,
+        return array(3.01,
             "Authenticates against an LDAP server",
             "angelnu",
             true);
